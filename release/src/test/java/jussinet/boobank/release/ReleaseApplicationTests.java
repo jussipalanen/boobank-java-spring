bc6cfcc2a237cce4c54da4dab618062b1804b220
Jussi Alanen <juzapala@gmail.com>
2023-07-11 15:01:45 +0300

    Init

---
+++ release/src/test/java/jussinet/boobank/release/ReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package jussinet.boobank.release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
